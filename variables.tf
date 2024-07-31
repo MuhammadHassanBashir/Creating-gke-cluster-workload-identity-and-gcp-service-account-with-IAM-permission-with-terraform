6f3b96e1daf73dd1d60387d68031273b92e94b4a
MuhammadHassanBashir <95129233+MuhammadHassanBashir@users.noreply.github.com>
2024-07-31 17:03:37 +0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "projectName" {
+  description = "Name of the Project"
+  type        = string
+  default     = "project_id" #In my case it is "world-learning-400909"
+}
+
+variable "region" {
+  description = "Name of the Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Name of the Location"
+  type        = string
+  default     = "us-central1-c"
+}
